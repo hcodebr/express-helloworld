eacd751ac05f13ab9433b94e160737a46456a996
Joao Rangel <joaohcrangel@gmail.com>
2023-02-02 12:41:32 -0300

    dockerfile

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const os = require('os');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log(process.env);
+  res.send(`Hello Hcode. ${os.type()} ${os.release()} ${os.platform()}`);
 });
 
 app.listen(port, () => {
